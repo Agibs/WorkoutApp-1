188fd360fcb46329eb03e664584518dbb6459b33
ralphbolo <rbolo062@uottawa.ca>
2015-02-10 11:06:50 -0500

    fixed email and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ActiveRecord::Base
   
   has_many :workouts, dependent: :destroy
 
-  
+  validates :email, :email => true, :length => { :minimum => 6}
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
+
 end
 
